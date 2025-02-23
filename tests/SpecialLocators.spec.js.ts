f7a190d290aed4ae15e7a88f3a807360b6bbcf9e
Selorm Ahorlu <selormonray14@gmail.com>
2025-02-23 00:16:34 +0000

    special locators 2

--- tests/SpecialLocators.spec.js.ts
+++ tests/SpecialLocators.spec.js.ts
@@ -4,6 +4,10 @@ test('Playwright Special locators', async ({ page }) => {
 
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
     await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
+    await page.getByPlaceholder("Password").fill("abc123");
+    await page.getByRole("button", {name: 'Submit'}).click();
 
 
     //locator(css)
