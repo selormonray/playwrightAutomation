e85a4f2f24fe66770c42703279237481b22317fb
Selorm Ahorlu <selormonray14@gmail.com>
2025-03-02 01:05:44 +0000

    hovering an element

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -5,21 +5,19 @@ test("Pop Up Validations", async ({page}) => {
 
     const showHideSelector = await page.locator("#displayed-text");
     const hideSelector = await page.locator("#hide-textbox");
+    const confirmButton = await page.locator("#confirmbtn");
+    const mouseHoverButton = await page.locator("#mousehover");
 
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     await expect(showHideSelector).toBeVisible();
     await hideSelector.click();
     await expect(await page.locator("#displayed-text")).toBeHidden();
+    page.on('dialog', dialog => dialog.dismiss()); //handling Java/JS pop ups
 
+    // hovering over an element
+    await mouseHoverButton.hover();
 
-
-
-
-   /* await page.goto("https://google.com");
-    await page.goBack();
-    await page.goForward();
-*/
 
 
 
