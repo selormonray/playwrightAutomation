4ce57b8d485463ee614eb7740ce2727631ca09b4
Selorm Ahorlu <selormonray14@gmail.com>
2025-03-02 00:50:15 +0000

    added assertions for hidden/visible elements

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,13 +1,26 @@
-import {test} from "@playwright/test";
+import {expect, test} from "@playwright/test";
 
 
 test("Pop Up Validations", async ({page}) => {
 
+    const showHideSelector = await page.locator("#displayed-text");
+    const hideSelector = await page.locator("#hide-textbox");
+
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-    await page.goto("https://google.com");
+    await expect(showHideSelector).toBeVisible();
+    await hideSelector.click();
+    await expect(await page.locator("#displayed-text")).toBeHidden();
+
+
+
+
+
+   /* await page.goto("https://google.com");
     await page.goBack();
     await page.goForward();
+*/
+
 
 
 
