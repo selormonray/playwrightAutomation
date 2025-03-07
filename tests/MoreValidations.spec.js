a536a65cd8c763a9ede2301500886096771b961c
Selorm Ahorlu <selormonray14@gmail.com>
2025-03-07 13:58:29 +0000

    logging the content

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -23,6 +23,8 @@ test("Pop Up Validations", async ({page}) => {
     await framesPage.locator("li a[href*='lifetime-access']:visible").click();
     const textCheck = await framesPage.locator(".text h2").textContent();
 
+    console.log(textCheck.split(" ")[1]);
+
 
 
 
