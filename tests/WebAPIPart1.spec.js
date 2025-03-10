f823d59649e4a830f56d6673a47af14e3acf9932
Selorm Ahorlu <selormonray14@gmail.com>
2025-03-10 00:09:02 +0000

    refactored the beforeAll Class

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,76 +1,15 @@
-const {test, expect, request} = require("@playwright/test");
+const { test, expect, request } = require("@playwright/test");
+const APIUtils = require("./utils/APIUtils");
 
+const loginPayLoad = { userEmail: "selormonray14@gmail.com", userPassword: "playwrightTester14" };
+const orderPayLoad = { orders: [{ country: "Ghana", productOrderedId: "67a8dde5c0d3e6622a297cc8" }] };
+let response;
 
-const loginPayLoad = {userEmail: "selormonray14@gmail.com", userPassword: "playwrightTester14"};
-const orderPayLoad = {orders: [{country: "Ghana", productOrderedId: "67a8dde5c0d3e6622a297cc8"}]};
-let token;
-let orderID;
+test.beforeAll(async ({ request }) => {
+    const apiUtils = new APIUtils(request, loginPayLoad);
+    response = await apiUtils.createOrder(orderPayLoad);
 
-/*
-*  Sets up an API context
-*  Sends a POST request to log in
-*  Ensures the response is successful
-*  Extracts and stores the authentication token
-* */
-
-test.beforeAll(async () => {
-    //login API
-    const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-        {data: loginPayLoad}
-    );
-
-    expect(loginResponse.ok()).toBeTruthy();
-
-    const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
-
-    //creating an order
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-        {
-            data: orderPayLoad,
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": token
-            },
-        })
-    //get the entire order json and save it in the orderResponseJson
-    const orderResponseJson = await orderResponse.json();
-    // get the orderID from the orderResponseJson
-    console.log(orderResponseJson);
-    orderID = orderResponseJson.orders[0];
-
+    expect(response).toHaveProperty("token");
+    expect(response).toHaveProperty("orderID");
 });
 
-
-test("Place Order", async ({page}) => {
-    const automationTestPracticeTextSelector = page.locator("div[class='left mt-1'] p");
-    const redBlinkTextSelector = page.locator(".m-2.blink_me");
-    const ordersSelector = page.locator(".btn.btn-custom[routerlink='/dashboard/myorders']");
-    const orderListSelector = page.locator("tbody .ng-star-inserted");
-
-
-    // insert token in local storage before the page loads thereby bypassing the login step
-    await page.addInitScript(value => {
-        window.localStorage.setItem("token", value);
-    }, token);
-    await page.goto("https://rahulshettyacademy.com/client");
-
-    await expect(automationTestPracticeTextSelector).toContainText("Automation Practice");
-    await expect(redBlinkTextSelector).toHaveText("User can only see maximum 9 products on a page");
-
-    await ordersSelector.click();
-    await orderListSelector.first().waitFor();
-    const rows = await page.locator("tbody tr");
-
-    for (let i = 0; i < await rows.count(); ++i) {
-        const rowOrderId = await rows.nth(i).locator("th").textContent();
-        if (orderID.includes(rowOrderId)) {
-            await rows.nth(i).locator("button").first().click();
-            break;
-        }
-    }
-    const orderIdDetails = await page.locator(".col-text").textContent();
-    expect(orderID.includes(orderIdDetails)).toBeTruthy();
-
-});
