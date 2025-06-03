8379d7cd0adcadff7813004efba8f2f0fbe18936
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-03 21:03:11 +0000

    added the fake payload order

--- tests/NetworkTest.spec.js
+++ tests/NetworkTest.spec.js
@@ -27,12 +27,13 @@ test("Place Order", async ({page}) => {
 
     await expect(automationTestPracticeTextSelector).toContainText("Automation Practice");
     await expect(redBlinkTextSelector).toHaveText("User can only see maximum 9 products on a page");
-
+    const fakePayloadOrders = {data: [], message: "No Orders"}
 
     // mock the Orders call
     page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/67b7369fc019fb1ad6036203",
         route => {
             const response = page.request.fetch(route.request());
+            let body = fakePayloadOrders;
             // intercepting response - API response -> browser ->render data on front end
 
         })
