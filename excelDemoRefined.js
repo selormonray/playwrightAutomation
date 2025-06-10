9da7593b88d3cecd7b9d22fb0f771aff88514047
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-10 23:22:28 +0000

    excel 2

--- excelDemoRefined.js
+++ excelDemoRefined.js
@@ -4,6 +4,15 @@ async function excelTest() {
     const workbook = new ExcelJs.Workbook();
     await workbook.xlsx.readFile("/Users/selormahorlu/Downloads/exceldownloadTest.xlsx");
 
+
+    // Check sheet name
+    workbook.eachSheet((sheet) => {
+        console.log(`Found sheet: ${sheet.name}`);
+    });
+
+
+
+
 }
 
 excelTest();
