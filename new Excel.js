93aef8691d3681ab992e58f4d112a3e90aa5943f
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-11 23:24:08 +0000

    get the path for the doc

--- new Excel.js
+++ new Excel.js
@@ -3,6 +3,13 @@ const ExcelJs = require('exceljs');
 
 async function excelTest() {
 
+    const workbook = new ExcelJs.Workbook();
+    await workbook.xlsx.readFile("/Users/selormahorlu/Downloads/exceldownloadTest.xlsx");
+
+
+}
+
+)
 
 }
 
