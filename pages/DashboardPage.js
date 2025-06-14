0e9aaec04ae1904135f15fcb13c740f1fab1bc61
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-14 20:30:19 +0000

    updated the search function

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -1,30 +1,28 @@
 class DashboardPage {
 
     constructor(page) {
-        this.roducts = page.locator(".card-body");
+        this.products = page.locator(".card-body");
         this.successSelector = page.locator("div[aria-label='Product Added To Cart']");
         this.cartSelector = page.locator("[routerlink='/dashboard/cart']");
         this.cartItemSelector = page.locator("div li");
-        this.products = page.locator(".card-body");
         this.productName = "IPHONE 13 PRO";
 
     }
 
-    searchProducts() {
+    async searchProducts(productName) {
 
-        await products.last().waitFor();
-        const productsCount = await products.count();
+        await this.products.last().waitFor();
+        const productsCount = await this.products.count();
         // loop through the products on the page and click on the product you desire
         for (let i = 0; i < productsCount; i++) {
-            if (await products.nth(i).locator("b").textContent() === productName) {
-                await products.nth(i).locator("text= Add To Cart").click();
+            if (await this.products.nth(i).locator("b").textContent() === productName) {
+                await this.products.nth(i).locator("text= Add To Cart").click();
                 break;
             }
         }
 
 
     }
-
 
 
 
