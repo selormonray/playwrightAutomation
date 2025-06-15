fe9cd692b07c8f0ba4751e08bb996cbf9d130a71
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-15 00:17:31 +0000

    added the locators

--- pages/CheckoutPage.js
+++ pages/CheckoutPage.js
@@ -3,6 +3,24 @@ const {expect} = require("@playwright/test");
 class CheckoutPage {
 
 
+    constructor(page) {
+        this.countrySelector = page.locator("input[placeholder='Select Country']");
+        this.cvvCodeSelector = page.locator("input[type='text']").nth(1);
+        this.nameOnCardSelector = page.locator("input[type='text']").nth(2);
+
+        this.successSelector = page.locator("div[aria-label='Product Added To Cart']");
+        this.placeOrderButton = page.locator(".btnn.action__submit");
+        this.thankYouOrderSelector = page.locator(".hero-primary");
+        this.thankYouText = " Thankyou for the order. ";
+        this.ordersSelector = page.locator(".btn.btn-custom[routerlink='/dashboard/myorders']");
+        this.orderListSelector = page.locator("tbody .ng-star-inserted");
+        this.orderIdSelector = page.locator("label[class='ng-star-inserted']");
+        this.countryDropdownOptions = page.locator(".ta-results");
+    }
+
+
+
+
 }
 
 module.exports = {CheckoutPage};
