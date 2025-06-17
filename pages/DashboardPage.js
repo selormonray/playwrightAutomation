95134d9eaaeea6218f42bc78ef320af5427f9b7e
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-17 22:35:16 +0000

    search product to cart

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -12,6 +12,19 @@ class DashboardPage {
     }
 
 
+    async searchAndAddProductsToCart(productName) {
+        await this.products.last().waitFor();
+        const productsCount = await this.products.count();
+        // loop through the products on the page and click on the product you desire
+        for (let i = 0; i < productsCount; i++) {
+            if (await this.products.nth(i).locator("b").textContent() === productName) {
+                await this.products.nth(i).locator("text= Add To Cart").click();
+                break;
+            }
+        }
+    }
+
+
 
 }
 
