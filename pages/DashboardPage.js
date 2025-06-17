834e547b8ebaa140e734819d88d1f15a353fd49d
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-17 22:34:53 +0000

    dashboard locators

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -2,6 +2,16 @@ const {expect} = require("@playwright/test");
 
 class DashboardPage {
 
+    constructor(page) {
+        this.products = page.locator(".card-body");
+        this.cartSelector = page.locator("[routerlink='/dashboard/cart']");
+        this.cartItemSelector = page.locator("div li");
+        this.automationTestPracticeTextSelector = page.locator("div[class='left mt-1'] p");
+        this.redBlinkTextSelector = page.locator(".m-2.blink_me");
+
+    }
+
+
 
 }
 
