cabdd80bf5559462a58fce46607cf253920e78cc
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-19 22:24:25 +0000

    added the locators

--- pages/OrdersHistoryPage.js
+++ pages/OrdersHistoryPage.js
@@ -1,5 +1,10 @@
 class OrdersHistoryPage {
-
+    constructor(page) {
+        this.page = page;
+        this.ordersTable = page.locator("tbody");
+        this.rows = page.locator("tbody tr");
+        this.orderdIdDetails = page.locator(".col-text");
+    }
 
 }
 
