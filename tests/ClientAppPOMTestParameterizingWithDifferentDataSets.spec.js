dea8a37ce5bddcf888250e745bcd8fa378ff27eb
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-21 22:59:59 +0000

    added parameterization with different data set

---
+++ tests/ClientAppPOMTestParameterizingWithDifferentDataSets.spec.js
@@ -0,0 +1,42 @@
+const {test, expect} = require("@playwright/test");
+const {POManager} = require("../pages/POManager");
+
+const loginDataSet = JSON.parse(JSON.stringify(require("../DTOs/loginDTO.json")));
+const userDetailsDataSet = JSON.parse(JSON.stringify(require("../DTOs/userDetailsDTO.json")));
+
+test("Client App Login", async ({page}) => {
+
+    const poManager = new POManager(page);
+    const loginPage = poManager.getLoginPage();
+    const dashboardPage = poManager.getDashboardPage();
+    const cartPage = poManager.getCartPage();
+    const checkoutPage = poManager.getCheckoutPage();
+    const ordersReviewPage = poManager.getOrdersReviewPage();
+    const orderHistoryPage = poManager.getOrdersHistoryPage();
+
+
+    await loginPage.goTo();
+    await loginPage.validLogin(loginDataSet.email, loginDataSet.password);
+
+    await dashboardPage.dashBoardPageVerifications(page);
+    await dashboardPage.searchAndAddProductsToCart(userDetailsDataSet.productName);
+    await dashboardPage.navigateToCart();
+
+    await cartPage.verifyPresenceOfSelectedIPhone();
+    await cartPage.navigateToCheckoutPage();
+    console.log(userDetailsDataSet.cvvCode);
+
+    await checkoutPage.fillForms(userDetailsDataSet.cvvCode, userDetailsDataSet.nameOnCardText);
+    await checkoutPage.placeOrder();
+    await checkoutPage.verifySuccessfullyPlacedOrder();
+
+    const orderId = await ordersReviewPage.getOrderID();
+
+    await ordersReviewPage.verifyReturnedOrderId(orderId);
+
+    await ordersReviewPage.placeOrder();
+
+    await orderHistoryPage.searchOrderAndSelect(orderId);
+    expect(orderId.includes(await orderHistoryPage.getOrderId())).toBeTruthy();
+
+});
