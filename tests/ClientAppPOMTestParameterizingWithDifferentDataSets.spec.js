e96b81f5413f307292ae8d3baa1e345e72dec61e
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-21 23:08:03 +0000

    updated the parameterization test

--- tests/ClientAppPOMTestParameterizingWithDifferentDataSets.spec.js
+++ tests/ClientAppPOMTestParameterizingWithDifferentDataSets.spec.js
@@ -1,42 +1,45 @@
 const {test, expect} = require("@playwright/test");
 const {POManager} = require("../pages/POManager");
 
-const loginDataSet = JSON.parse(JSON.stringify(require("../DTOs/loginDTO.json")));
-const userDetailsDataSet = JSON.parse(JSON.stringify(require("../DTOs/userDetailsDTO.json")));
+const dataset = JSON.parse(JSON.stringify(require("../DTOs/loginDTO.json")));
+const data2 = JSON.parse(JSON.stringify(require("../DTOs/userDetailsDTO.json")));
 
-test("Client App Login", async ({page}) => {
+for (const data of dataset) {
+    test(`Client App Login Parameterization ${data.email}`, async ({page}) => {
 
-    const poManager = new POManager(page);
-    const loginPage = poManager.getLoginPage();
-    const dashboardPage = poManager.getDashboardPage();
-    const cartPage = poManager.getCartPage();
-    const checkoutPage = poManager.getCheckoutPage();
-    const ordersReviewPage = poManager.getOrdersReviewPage();
-    const orderHistoryPage = poManager.getOrdersHistoryPage();
+        const poManager = new POManager(page);
+        const loginPage = poManager.getLoginPage();
+        const dashboardPage = poManager.getDashboardPage();
+        const cartPage = poManager.getCartPage();
+        const checkoutPage = poManager.getCheckoutPage();
+        const ordersReviewPage = poManager.getOrdersReviewPage();
+        const orderHistoryPage = poManager.getOrdersHistoryPage();
 
 
-    await loginPage.goTo();
-    await loginPage.validLogin(loginDataSet.email, loginDataSet.password);
+        await loginPage.goTo();
+        await loginPage.validLogin(data.email, data.password);
 
-    await dashboardPage.dashBoardPageVerifications(page);
-    await dashboardPage.searchAndAddProductsToCart(userDetailsDataSet.productName);
-    await dashboardPage.navigateToCart();
+        await dashboardPage.dashBoardPageVerifications(page);
+        await dashboardPage.searchAndAddProductsToCart(userDetailsDataSet.productName);
+        await dashboardPage.navigateToCart();
 
-    await cartPage.verifyPresenceOfSelectedIPhone();
-    await cartPage.navigateToCheckoutPage();
-    console.log(userDetailsDataSet.cvvCode);
+        await cartPage.verifyPresenceOfSelectedIPhone();
+        await cartPage.navigateToCheckoutPage();
+        console.log(userDetailsDataSet.cvvCode);
 
-    await checkoutPage.fillForms(userDetailsDataSet.cvvCode, userDetailsDataSet.nameOnCardText);
-    await checkoutPage.placeOrder();
-    await checkoutPage.verifySuccessfullyPlacedOrder();
+        await checkoutPage.fillForms(userDetailsDataSet.cvvCode, userDetailsDataSet.nameOnCardText);
+        await checkoutPage.placeOrder();
+        await checkoutPage.verifySuccessfullyPlacedOrder();
 
-    const orderId = await ordersReviewPage.getOrderID();
+        const orderId = await ordersReviewPage.getOrderID();
 
-    await ordersReviewPage.verifyReturnedOrderId(orderId);
+        await ordersReviewPage.verifyReturnedOrderId(orderId);
 
-    await ordersReviewPage.placeOrder();
+        await ordersReviewPage.placeOrder();
 
-    await orderHistoryPage.searchOrderAndSelect(orderId);
-    expect(orderId.includes(await orderHistoryPage.getOrderId())).toBeTruthy();
+        await orderHistoryPage.searchOrderAndSelect(orderId);
+        expect(orderId.includes(await orderHistoryPage.getOrderId())).toBeTruthy();
 
-});
+    });
+}
+
