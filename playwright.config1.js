e30465a8731307c116494b80b264830dd77c4b4c
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-23 23:38:24 +0000

    added and updated the playwright config to run different test browsers

---
+++ playwright.config1.js
@@ -0,0 +1,64 @@
+// @ts-check
+import {defineConfig} from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+    testDir: "./tests",
+
+    /* Maximum time one test can run for. */
+    timeout: 30 * 1000,
+    expect: {
+        timeout: 5000,
+    },
+
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+
+    reporter: "html",
+
+    projects: [
+        {
+            name: "Chrome Execution",
+            use: {
+                // use helps you select what browser you want to run, to get screenshots, logs. you enter everything here
+                browserName: "chromium",
+                headless: false,
+                screenshot: "only-on-failure",
+                trace: 'retain-on-failure',
+                // trace: 'on',
+            }
+        },
+        {
+            name: "Safari Execution",
+            use: {
+                // use helps you select what browser you want to run, to get screenshots, logs. you enter everything here
+                browserName: "webkit",
+                headless: false,
+                screenshot: "only-on-failure",
+                trace: 'retain-on-failure',
+                // trace: 'on',
+            }
+        }
+
+    ]
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //   command: 'npm run start',
+    //   url: 'http://127.0.0.1:3000',
+    //   reuseExistingServer: !process.env.CI,
+    // },
+});
