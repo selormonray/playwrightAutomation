f0c3704fc72c2d74b794603658b22d95b99d8eb3
Selorm Ahorlu <selormonray14@gmail.com>
2025-06-25 22:12:56 +0000

    added retries for helping flaky tests

--- playwright.config1.js
+++ playwright.config1.js
@@ -14,6 +14,7 @@ import {defineConfig, devices} from "@playwright/test";
  */
 export default defineConfig({
     testDir: "./tests",
+    retries: 2,
 
     /* Maximum time one test can run for. */
     timeout: 30 * 1000,
