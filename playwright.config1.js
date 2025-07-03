c0e3c26880c05ca861dcdb896f4e7d032f55fab5
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-03 23:23:37 +0000

    adds number of test retries

--- playwright.config1.js
+++ playwright.config1.js
@@ -14,8 +14,7 @@ import {defineConfig} from "@playwright/test";
  */
 export default defineConfig({
     testDir: "./tests",
-    retries: 2,
-
+    retries: 1
     /* Maximum time one test can run for. */
     timeout: 30 * 1000,
     expect: {
