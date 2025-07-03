b4bfde23186a2a478338ce307ab282810c51781e
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-03 23:26:41 +0000

    tests updated

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -87,5 +87,14 @@ test("Client App Login", async ({page}) => {
     console.log(orderId);
     const rows = await page.locator("tbody tr");
 
+    for (let i = 0; i < await rows.count(); ++i) {
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
 });
