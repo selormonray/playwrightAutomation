cc0a66ce7c5103439963da2c4532a6a4ed2d2e39
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-04 16:18:10 +0000

    updates the selectors for visual screenshots

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -24,6 +24,11 @@ test("Pop Up Validations", async ({page}) => {
 
 test("Screenshot & Visual Comparison", async ({page}) => {
 
+    const showHideSelector = await page.locator("#displayed-text");
+    const hideSelector = await page.locator("#hide-textbox");
+    const confirmButton = await page.locator("#confirmbtn");
+    const mouseHoverButton = await page.locator("#mousehover");
+    const frameLocator = await page.locator("#courses-iframe");
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     await expect(showHideSelector).toBeVisible();
