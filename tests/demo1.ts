4da75e029b6afdfd16d126c817f3db14fe63fa0b
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-13 21:43:17 +0000

    updates the array

--- tests/demo1.ts
+++ tests/demo1.ts
@@ -4,7 +4,9 @@ message1 = "hellooo";
 let age1: number = 25;
 let isActive1: boolean = false;
 
-let numberArray1: number[] = [1, 2, 3, 4, 5];
 
 let data1: any = "This is a string";
 data1 = 42;
+
+
+let numberArray1: number[] = [1, 2, 3, 4, 5];
