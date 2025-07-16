04605736c708ae7594f9bd6249e00ddc70908d33
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-16 23:40:19 +0000

    added locators for ts

---
+++ pages_ts/CartPage.ts
@@ -0,0 +1,22 @@
+import {Locator, Page} from '@playwright/test';
+
+export class CartPage {
+
+    cartProducts: Locator;
+    productsText: Locator;
+    cart: Locator;
+    orders: Locator;
+    checkout: Locator;
+    page: Page;
+
+
+    constructor(page: Page) {
+        this.page = page;
+        this.cartProducts = page.locator("div li").first();
+        this.productsText = page.locator(".card-body b");
+        this.cart = page.locator("[routerlink*='cart']");
+        this.orders = page.locator("button[routerlink*='myorders']");
+        this.checkout = page.locator("text=Checkout");
+
+    }
+}
