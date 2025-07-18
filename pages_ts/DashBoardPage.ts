ec97679e7dec160a654bd268c7bf806975e331ba
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-18 23:59:40 +0000

    adds cons

--- pages_ts/DashBoardPage.ts
+++ pages_ts/DashBoardPage.ts
@@ -7,6 +7,16 @@ export class DashboardPage {
 
     page: Page;
 
+    constructor(page: Page) {
+        this.page = page;
+        this.products = page.locator(".card-body");
+        this.productsText = page.locator(".card-body b");
+        this.cart = page.locator("[routerlink*='cart']");
+        this.orders = page.locator("button[routerlink*='myorders']");
+
+    }
+
+
 }
 
 module.exports = {DashboardPage};
