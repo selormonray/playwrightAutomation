95d3ee510104b844c815cec7e1824100a484d02d
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-23 20:19:18 +0000

    created a new definition test file

---
+++ features/step_definitions/my_steps.js
@@ -0,0 +1,6 @@
+const {Given, When, Then, Before, After} = require("@cucumber/cucumber");
+const {POManager} = require("pages/POManager");
+const {chromium} = require("@playwright/test");
+const loginDataSet = JSON.parse(JSON.stringify(require("DTOs/loginDTO.json")));
+const userDetailsDataSet = JSON.parse(JSON.stringify(require("DTOs/userDetailsDTO.json")));
+
