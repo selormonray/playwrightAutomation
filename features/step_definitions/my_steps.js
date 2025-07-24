72420d3c8765d89089e7fbcf07765789064ece5e
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-24 23:38:25 +0000

    finalized the step definition method

--- features/step_definitions/my_steps.js
+++ features/step_definitions/my_steps.js
@@ -50,4 +50,9 @@ When('I enter valid details and place order', async function () {
 
 Then('Verify order is present in the order history page', async function () {
     const orderHistoryPage = poManager.getOrdersHistoryPage();
-
+    await orderHistoryPage.searchOrderAndSelect(orderId);
+    const historyOrderId = await orderHistoryPage.getOrderId();
+    if (!orderId.includes(historyOrderId)) {
+        throw new Error('Order not found in history');
+    }
+});
