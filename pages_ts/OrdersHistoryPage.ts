f934cc466d726f1a55ca0d9c384a8080ac0f56e6
Selorm Ahorlu <selormonray14@gmail.com>
2025-07-25 22:53:36 +0000

    adds the constructor

--- pages_ts/OrdersHistoryPage.ts
+++ pages_ts/OrdersHistoryPage.ts
@@ -5,6 +5,16 @@ pageimport
 from
 '@playwright/test';
 
-export class OrdersHistoryPage {
+export class OrdersHistoryPage
+{
+    orderdIdDetails: Locator;
+    rows: Locator;
+    ordersTable: Locator;
+    page: Page;
+
+    constructor(page: Page)
+
 
 }
+
+module.exports = {OrdersHistoryPage};
