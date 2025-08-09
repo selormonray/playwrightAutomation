90184a68a669265ebfc9e838907b2d9d88d0d671
Selorm Ahorlu <selormonray14@gmail.com>
2025-08-09 23:56:34 +0000

    rewrote the class 3

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -11,3 +11,9 @@ const orderPayload = {
     orders: [{country: "Ghana", productOrderedId: "67a8dde5c0d3e6622a297cc8"}],
 };
 
+let apiResponse;
+
+test.beforeAll(async ({request}) => {
+    const apiUtils = new APIUtils(request, loginPayload);
+    apiResponse = await apiUtils.createOrder(orderPayload);
+
