b10013b4c72c38ca7d0bea5797910f1e3badb36e
Selorm Ahorlu <selormonray14@gmail.com>
2025-08-19 23:58:53 +0000

    update 6

--- pages_ts/LoginPage.ts
+++ pages_ts/LoginPage.ts
@@ -19,7 +19,6 @@ export class LoginPage {
 
     async validLogin(username: string, password: string) {
         await this.userName.fill(username);
-
         await this.password.fill(password);
         await this.signInbutton.click();
         await this.page.waitForLoadState('networkidle');
