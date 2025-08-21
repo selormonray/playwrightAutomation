122f0fdf88d3f9a563815ce2fed059661150bf36
Selorm Ahorlu <selormonray14@gmail.com>
2025-08-21 23:58:18 +0000

    updating valid details

--- features/step_definitions/my_steps.js
+++ features/step_definitions/my_steps.js
@@ -37,6 +37,7 @@ Then('Verify {string} is added to the cart', async function (productName) {
 });
 
 When('I enter valid details and place order', async function () {
+    const checkoutPage = poManager.getCheckoutPage();
 
     const ordersReviewPage = poManager.getOrdersReviewPage();
     orderId = await ordersReviewPage.getOrderID();
