c824942b6d7e1a7eb12e8212686cbf83eeda9a93
Selorm Ahorlu <selormonray14@gmail.com>
2025-08-28 00:00:39 +0000

    added the product name

--- features/step_definitions/my_steps.js
+++ features/step_definitions/my_steps.js
@@ -27,6 +27,8 @@ Given('a login to the ecommerce application using {string} and {string}', async 
 When('I add {string} to the cart', async function (productName) {
     const dashboardPage = poManager.getDashboardPage();
     await dashboardPage.dashBoardPageVerifications(page);
+    await dashboardPage.searchAndAddProductsToCart(productName);
+});
 Then('Verify {string} is added to the cart', async function (productName) {
     const cartPage = poManager.getCartPage();
     await cartPage.verifyPresenceOfSelectedIPhone();
